c89d719bcf444dcab01ed4efbbc85d4c24f946ed
Landon Turner <lturner@teamworks.com>
2020-07-27 11:20:36 -0400

    grid rendered to ui

--- canvas/src/App.js
+++ canvas/src/App.js
@@ -17,11 +17,31 @@ function App() {
     return rows;
   })
 
-  console.log(grid)
+  // later on try to break this into styled components to clean up the main component
 
   return(
     <div className="App">
       <h1>Lando's Game of Life</h1>
+      {/* style the grid so it's a square */}
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px)`
+      }}>
+        {/* map through grid to make rows and chain to make cols */}
+        {grid.map((rows, i) => 
+          rows.map((col, k) => (
+            <div
+            key={`${i}-${k}`} 
+            style={{
+              width: 20, 
+              height: 20, 
+              backgroundColor: grid[i][k] ? 'black' : undefined, 
+              border: "solid 1px black"
+              }} 
+            />
+          ))
+        )}
+      </div>
     </div>
   ) 
 }
